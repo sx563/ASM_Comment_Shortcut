795673b0976c8165da0f524698ac2cfdb67dd438
Susanna Xu <sx563@nyu.edu>
2019-12-07 23:24:31 -0500

    can comment/uncomment multiple lines of code at once now

--- main.js
+++ main.js
@@ -23,36 +23,61 @@ define([
 	};
 
 
-	function toggleEmu86KernelComment(){
-        var comment_token = ';';
+	var comment_token = ";";
+	
+	function executetoggleComment(job, cm, start_line_cursor, start_text_cursor){
+		if(job === "uncommentLine" ){
+			cm.doc.setCursor(start_text_cursor);
+			CodeMirror.commands.delCharAfter(cm);
+		}
+		if(job === "commentLine"){
+			cm.doc.setCursor(start_line_cursor);
+			cm.doc.replaceSelection(comment_token);
+		}
+	}
 
-        var cm = Jupyter.notebook.get_selected_cell().code_mirror
-		
-		var start_cursor = cm.doc.getCursor();
-        CodeMirror.commands.goLineEnd(cm);
-        CodeMirror.commands.goLineStartSmart(cm);
-
-        var start_line_cursor = cm.doc.getCursor();
-		var start_line = {'line': start_line_cursor.line, 'ch': start_line_cursor.ch};
-
-        CodeMirror.commands.goLineEnd(cm);
-        var end_line_cursor = cm.doc.getCursor();
-		var end_line = {'line': end_line_cursor.line, 'ch': end_line_cursor.ch};
-		
-        
-        var line = cm.doc.getRange(start_line, end_line);
-		CodeMirror.commands.deleteLine(cm);
-		cm.doc.setCursor(start_cursor.line, start_cursor.ch);
-        if(line[0] === comment_token){
-            cm.doc.replaceSelection(line.slice(1) + "\n");
-        }else{
-            cm.doc.replaceSelection(comment_token + line + "\n");
+	function iterateThroughSelectedTextDoingJob(job, cm, from_cursor, to_cursor){
+		cm.doc.setCursor(from_cursor);
+		var cursor_line = -1;
+		while(to_cursor.line != cursor_line){
+			CodeMirror.commands.goLineStart(cm);
+			var start_line_cursor = cm.doc.getCursor();
+			
+			CodeMirror.commands.goLineEnd(cm);
+			CodeMirror.commands.goLineStartSmart(cm);
+			var start_text_cursor = cm.doc.getCursor();
+			
+			CodeMirror.commands.goLineEnd(cm);
+			var end_text_cursor = cm.doc.getCursor();
+			cursor_line = end_text_cursor.line;
+			
+			var line = cm.doc.getRange(start_text_cursor, end_text_cursor);
+			if(line.trim() !== ""){
+				if(job === "determineCommentOrUncomment"){
+					if(line[0] !== comment_token){
+						return "commentLine";
+					}
+				}else{
+					executetoggleComment(job, cm, start_line_cursor, start_text_cursor);
+				}
+			}
+			
+            cm.doc.setCursor(start_line_cursor.line + 1, start_line_cursor.ch);
         }
+		return "uncommentLine";
+	}
 
-        cm.doc.setCursor(start_cursor.line, start_cursor.ch);
-        
+    function toggleEmu86KernelComment(){
+		var cm = Jupyter.notebook.get_selected_cell().code_mirror
+		
+		var from_cursor = cm.doc.getCursor("from");
+		var to_cursor = cm.doc.getCursor("to");
+		
+		var toggleCommentJob = iterateThroughSelectedTextDoingJob("determineCommentOrUncomment", cm, from_cursor, to_cursor);
+		iterateThroughSelectedTextDoingJob(toggleCommentJob, cm, from_cursor, to_cursor);
+
+		cm.doc.setSelection(from_cursor, to_cursor);
     }
-
 
     return {
         load_ipython_extension: Jupyter.keyboard_manager.edit_shortcuts.add_shortcuts(edit_mode_shortcut),
