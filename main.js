764933be7175d6f3f67a51cd84e5b120d3f6a3b8
Susanna Xu <sx563@nyu.edu>
2019-12-10 19:28:42 -0500

    change scope of variables

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ define([
 ], function(Jupyter, CodeMirror) {
 	"use strict";
 
-	var prefix = 'auto';
+	const prefix = 'auto';
 
-	var action_toggleEmu86KernelComment = Jupyter.keyboard_manager.actions.register(
+	const action_toggleEmu86KernelComment = Jupyter.keyboard_manager.actions.register(
 		{
 		icon: 'fa-comment-o',
 		help: 'Toggle Emu86 Kernel Comment',
@@ -18,12 +18,12 @@ define([
 		prefix
 	);
 
-	var edit_mode_shortcut = {
+	const edit_mode_shortcut = {
 		'Ctrl-Alt-e': action_toggleEmu86KernelComment
 	};
 
 
-	var comment_token = ";";
+	const comment_token = ";";
 
 	function executetoggleComment(job, cm, start_line_cursor, start_text_cursor){
 		if(job === "uncommentLine" ){
@@ -38,20 +38,20 @@ define([
 
 	function iterateThroughSelectedTextDoingJob(job, cm, from_cursor, to_cursor){
 		cm.doc.setCursor(from_cursor);
-		var cursor_line = -1;
+		let cursor_line = -1;
 		while(to_cursor.line != cursor_line){
 			CodeMirror.commands.goLineStart(cm);
-			var start_line_cursor = cm.doc.getCursor();
+			let start_line_cursor = cm.doc.getCursor();
 
 			CodeMirror.commands.goLineEnd(cm);
 			CodeMirror.commands.goLineStartSmart(cm);
-			var start_text_cursor = cm.doc.getCursor();
+			let start_text_cursor = cm.doc.getCursor();
 
 			CodeMirror.commands.goLineEnd(cm);
-			var end_text_cursor = cm.doc.getCursor();
+			let end_text_cursor = cm.doc.getCursor();
 			cursor_line = end_text_cursor.line;
 
-			var line = cm.doc.getRange(start_text_cursor, end_text_cursor);
+			let line = cm.doc.getRange(start_text_cursor, end_text_cursor);
 			if(line.trim() !== ""){
 				if(job === "determineCommentOrUncomment"){
 					if(line[0] !== comment_token){
@@ -68,12 +68,12 @@ define([
 	}
 
 	function toggleEmu86KernelComment(){
-		var cm = Jupyter.notebook.get_selected_cell().code_mirror
+		const cm = Jupyter.notebook.get_selected_cell().code_mirror
 
-		var from_cursor = cm.doc.getCursor("from");
-		var to_cursor = cm.doc.getCursor("to");
+		const from_cursor = cm.doc.getCursor("from");
+		const to_cursor = cm.doc.getCursor("to");
 
-		var toggleCommentJob = iterateThroughSelectedTextDoingJob("determineCommentOrUncomment", cm, from_cursor, to_cursor);
+		const toggleCommentJob = iterateThroughSelectedTextDoingJob("determineCommentOrUncomment", cm, from_cursor, to_cursor);
 		iterateThroughSelectedTextDoingJob(toggleCommentJob, cm, from_cursor, to_cursor);
 
 		cm.doc.setSelection(from_cursor, to_cursor);
